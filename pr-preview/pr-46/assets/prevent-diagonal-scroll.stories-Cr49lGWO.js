48bd1e3449f8c0f1bdfe9acff3a1eceabec701d4
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-03 08:55:47 +0000

    Deploy preview for PR 46 🛫

--- pr-preview/pr-46/assets/prevent-diagonal-scroll.stories-Cr49lGWO.js
+++
@@ -1,7 +0,0 @@
-import{R as e}from"./iframe-xfNT-Ps4.js";import{D as s}from"./data-editor-all-Bv2b1TfO.js";import{B as i,D as c,P as m,u as p,d as u}from"./utils-HAQNaOj6.js";import{S as g}from"./story-utils-DLvWRFCq.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-BhEiF8rE.js";import"./throttle-CZ4P-hpv.js";import"./marked.esm-CE7AGV3Y.js";import"./flatten-YzC76C5l.js";import"./scrolling-data-grid-CXN2be8I.js";import"./index-D_kXk1yT.js";import"./useResizeDetector-CBzhzuww.js";const w={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(g,null,e.createElement(i,{title:"Prevent Diagonal Scroll",description:e.createElement(e.Fragment,null,e.createElement(c,null,"Diagonal scrolling can be prevented by setting"," ",e.createElement(m,null,"preventDiagonalScrolling"),"."))},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:n}=p(200);return e.createElement(s,{...u,getCellContent:n,columns:r,preventDiagonalScrolling:!0,rows:5e3})};var o,a,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(200);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} preventDiagonalScrolling={true} rows={5000} />;
-}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const x=["PreventDiagonalScroll"];export{t as PreventDiagonalScroll,x as __namedExportsOrder,w as default};
--- pr-preview/pr-46/assets/small-editable-grid.stories-6V8EjGGu.js
+++
@@ -1,8 +0,0 @@
-import{R as e}from"./iframe-xfNT-Ps4.js";import{D as i}from"./data-editor-all-Bv2b1TfO.js";import{B as m,D as d,u as p,d as c}from"./utils-HAQNaOj6.js";import{S as u}from"./story-utils-DLvWRFCq.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-BhEiF8rE.js";import"./throttle-CZ4P-hpv.js";import"./marked.esm-CE7AGV3Y.js";import"./flatten-YzC76C5l.js";import"./scrolling-data-grid-CXN2be8I.js";import"./index-D_kXk1yT.js";import"./useResizeDetector-CBzhzuww.js";const _={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(u,null,e.createElement(m,{title:"Editable Grid",description:e.createElement(d,null,"Data grid supports overlay editors for changing values. There are bespoke editors for numbers, strings, images, booleans, markdown, and uri.")},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:s,setCellValue:n}=p(6,!1);return e.createElement(i,{...c,getCellContent:s,columns:r,rows:20,onCellEdited:n})};var o,a,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent,
-    setCellValue
-  } = useMockDataGenerator(6, false);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rows={20} onCellEdited={setCellValue} />;
-}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const v=["SmallEditableGrid"];export{t as SmallEditableGrid,v as __namedExportsOrder,_ as default};
