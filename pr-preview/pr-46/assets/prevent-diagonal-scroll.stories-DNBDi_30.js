df7202570fafabb68387254ec840207aaa69824a
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-03 08:22:28 +0000

    Deploy preview for PR 46 🛫

--- pr-preview/pr-46/assets/prevent-diagonal-scroll.stories-DNBDi_30.js
+++
@@ -1,7 +0,0 @@
-import{R as e}from"./iframe-gAOLjoLa.js";import{D as s}from"./data-editor-all-BGZjIGQw.js";import{B as i,D as c,P as m,u as p,d as u}from"./utils--O3IDUrL.js";import{S as g}from"./story-utils-B8ySLVQn.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-Dz22i1uq.js";import"./throttle-Bge67UJ4.js";import"./marked.esm-B7yOw_qN.js";import"./flatten-CQRU4ZyZ.js";import"./scrolling-data-grid-BRouUnBX.js";import"./index-D_kXk1yT.js";import"./useResizeDetector-DlNsOJfJ.js";const w={title:"Glide-Data-Grid/DataEditor Demos",decorators:[r=>e.createElement(g,null,e.createElement(i,{title:"Prevent Diagonal Scroll",description:e.createElement(e.Fragment,null,e.createElement(c,null,"Diagonal scrolling can be prevented by setting"," ",e.createElement(m,null,"preventDiagonalScrolling"),"."))},e.createElement(r,null)))]},t=()=>{const{cols:r,getCellContent:n}=p(200);return e.createElement(s,{...u,getCellContent:n,columns:r,preventDiagonalScrolling:!0,rows:5e3})};var o,a,l;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent
-  } = useMockDataGenerator(200);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} preventDiagonalScrolling={true} rows={5000} />;
-}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const x=["PreventDiagonalScroll"];export{t as PreventDiagonalScroll,x as __namedExportsOrder,w as default};
--- pr-preview/pr-46/assets/scaled-view.stories-GWK62oDq.js
+++
@@ -1,8 +0,0 @@
-import{R as e}from"./iframe-gAOLjoLa.js";import{D as i}from"./data-editor-all-BGZjIGQw.js";import{B as m,D as c,u as p,d}from"./utils--O3IDUrL.js";import{S as u}from"./story-utils-B8ySLVQn.js";import"./preload-helper-C1FmrZbK.js";import"./image-window-loader-Dz22i1uq.js";import"./throttle-Bge67UJ4.js";import"./marked.esm-B7yOw_qN.js";import"./flatten-CQRU4ZyZ.js";import"./scrolling-data-grid-BRouUnBX.js";import"./index-D_kXk1yT.js";import"./useResizeDetector-DlNsOJfJ.js";const M={title:"Glide-Data-Grid/DataEditor Demos",decorators:[o=>e.createElement(u,null,e.createElement(m,{title:"Scaled view",description:e.createElement(c,null,"The data editor supports being scaled."),scale:"0.5"},e.createElement(o,null)))]},t=()=>{const{cols:o,getCellContent:n,onColumnResize:l}=p(60);return e.createElement(i,{...d,getCellContent:n,columns:o,rowMarkers:"both",rows:500,onColumnResize:l})};var r,a,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
-  const {
-    cols,
-    getCellContent,
-    onColumnResize
-  } = useMockDataGenerator(60);
-  return <DataEditor {...defaultProps} getCellContent={getCellContent} columns={cols} rowMarkers="both" rows={500} onColumnResize={onColumnResize} />;
-}`,...(s=(a=t.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const b=["ScaledView"];export{t as ScaledView,b as __namedExportsOrder,M as default};
